225ab8097bd9a39029a633eb2e7307d0d68e73a5
Silvio Peroni <essepuntato@gmail.com>
2019-10-20 16:22:15 +0200

    added chapter 4

--- index.md
+++ index.md
@@ -6,3 +6,10 @@ This is the official book written for the *Computational Thinking and Programmin
 1. Introduction to Computational Thinking [[PDF]](book/01.pdf)
 2. Algorithms [[PDF]](book/02.pdf)
 3. Computability [[PDF]](book/03.pdf)
+4. Programming languages [[PDF]](book/04.pdf)
+
+**Python code available in each chapter**
+1. None
+2. None
+3. None
+4. [first_algorithm_empty.py](python/first_algorithm_empty.py), [first_algorithm_no_assignments.py](python/first_algorithm_no_assignments.py), [first_algorithm.py](python/first_algorithm.py) 
---
+++ python/first_algorithm.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+# Copyright (c) 2018, Silvio Peroni <essepuntato@gmail.com>
+#
+# Permission to use, copy, modify, and/or distribute this software for any purpose
+# with or without fee is hereby granted, provided that the above copyright notice
+# and this permission notice appear in all copies.
+#
+# THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES WITH
+# REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF MERCHANTABILITY AND
+# FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY SPECIAL, DIRECT, INDIRECT,
+# OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING FROM LOSS OF USE,
+# DATA OR PROFITS, WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS
+# ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS
+# SOFTWARE.
+
+
+# Test case for the algorithm
+def test_contains_word(first_word, second_word, bib_entry, expected):
+    result = contains_word(first_word, second_word, bib_entry)
+    if expected == result:
+        return True
+    else:
+        return False
+
+
+# Code of the algorithm
+def contains_word(first_word, second_word, bib_entry):
+    contains_first_word = first_word in bib_entry
+    contains_second_word = second_word in bib_entry
+
+    if contains_first_word and contains_second_word:
+        return 2
+    elif contains_first_word or contains_second_word:
+        return 1
+    else:
+        return 0
+
+
+# Three different test runs
+print(test_contains_word("Shotton", "Open",
+                         "Shotton, D. (2013). Open Citations. Nature, 502: 295–297. doi:10.1038/502295a", 2))
+print(test_contains_word("Citations", "Science",
+                         "Shotton, D. (2013). Open Citations. Nature, 502: 295–297. doi:10.1038/502295a", 1))
+print(test_contains_word("References", "1983",
+                         "Shotton, D. (2013). Open Citations. Nature, 502: 295–297. doi:10.1038/502295a", 0))
